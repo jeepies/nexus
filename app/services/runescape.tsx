5f610911f677dd3784f4a41bffde15e6b289f6e7
jay <jay@jeepies.codes>
2025-03-08 01:01:46 +0000

    feat: added fetchChatHeadPicture

--- app/services/runescape.tsx
+++ app/services/runescape.tsx
@@ -1,17 +1,32 @@
 import axios from 'axios';
 
-const api = axios.create({
+const appsApi = axios.create({
     baseURL: "https://apps.runescape.com",
     timeout: 5000,
     validateStatus: (status) => status < 500,
 })
 
+const secureApi = axios.create({
+    baseURL: "https://secure.runescape.com",
+    timeout: 5000,
+    validateStatus: (status) => status < 500,
+})
+
 export async function fetchRunemetrics(username: string, activityCount: number) {
-    const result = await api.get(`runemetrics/profile/profile`, {
+    const result = await appsApi.get(`runemetrics/profile/profile`, {
         params: {
             user: username,
             activities: activityCount
         }
     });
     return result.data;
+}
+
+export async function fetchChatHeadPicture(username: string) {
+    // i'm not sure why it cant decode the data from a redirect, will have to investigate (soon™️)
+    const parentResult = await secureApi.get(`/m=avatar-rs/${username}/chat.png`, { maxRedirects: 0 });
+    const childResult = await axios.get(parentResult.headers.location ?? "https://secure.runescape.com/m=avatar-rs/default_chat.png?", {
+        responseType: "arraybuffer",
+    })
+    return Buffer.from(childResult.data, 'binary').toString('base64');
 }
